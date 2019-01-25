57c7714c93f73dc65c51217195727bc6bab63bd5
connor <connormullett@gmail.com>
2019-01-25 15:25:15 -0500

    model trained on k49, 91% acc, 0.3 loss, evaluate.py shows hiragana prediction from class_map

--- evaluate.py
+++ evaluate.py
@@ -7,29 +7,28 @@ from tensorflow import keras
 
 import pandas as pd
 
+img_rows, img_cols = 28, 28
+input_shape = (img_rows, img_cols, 1)
+class_map = pd.read_csv('data/k49_classmap.csv')
+
+# -- Data format
 x_test = np.load('data/k49-test-imgs.npz')['arr_0']
 y_test = np.load('data/k49-test-labels.npz')['arr_0']
 
-img_rows, img_cols = 28, 28
-input_shape = (img_rows, img_cols, 1)
-
 x_test = x_test.astype('float32')
 x_test /= 255
-
 x_test = x_test.reshape(x_test.shape[0], *input_shape)
-
 y_test = to_categorical(y_test, 49)
+# --
 
 model = keras.models.load_model('model.h5')
 
-class_map = pd.read_csv('data/kmnist_classmap.csv')
+predictions = model.predict(x_test[:1000], verbose=1)
+prediction_test = predictions[0]
 
-print(x_test)
-model.predict(x_test[0])
+loss, acc = model.evaluate(x_test, y_test)
+print('acc: {}'.format(100 * acc))
 
-# loss, acc = model.evaluate(x_test, y_test)
-# print('acc: {}'.format(100 * acc))
-
-# csv = pd.read_csv('data/k49_classmap.csv')
-# print(csv.head(3))
+csv = pd.read_csv('data/k49_classmap.csv')
+print(csv.head(3))
 
