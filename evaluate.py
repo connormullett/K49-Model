6cac025258ca75c250629ae0de2e212d9db9e9ca
connor <connormullett@gmail.com>
2019-01-25 13:50:24 -0500

    trained on k49

--- evaluate.py
+++ evaluate.py
@@ -2,14 +2,34 @@
 import tensorflow as tf
 import numpy as np
 
+from keras.utils import to_categorical
 from tensorflow import keras
 
+import pandas as pd
 
 x_test = np.load('data/k49-test-imgs.npz')['arr_0']
 y_test = np.load('data/k49-test-labels.npz')['arr_0']
 
+img_rows, img_cols = 28, 28
+input_shape = (img_rows, img_cols, 1)
+
+x_test = x_test.astype('float32')
+x_test /= 255
+
+x_test = x_test.reshape(x_test.shape[0], *input_shape)
+
+y_test = to_categorical(y_test, 49)
+
 model = keras.models.load_model('model.h5')
 
-loss, acc = model.evaluate(x_test, y_test)
-print('acc: {}'.format(100 * acc))
+class_map = pd.read_csv('data/kmnist_classmap.csv')
+
+print(x_test)
+model.predict(x_test[0])
+
+# loss, acc = model.evaluate(x_test, y_test)
+# print('acc: {}'.format(100 * acc))
+
+# csv = pd.read_csv('data/k49_classmap.csv')
+# print(csv.head(3))
 
--- train.py
+++ train.py
@@ -6,10 +6,25 @@ from keras.layers import Conv2D, MaxPooling2D, Dense
 from keras.layers import Dropout, Flatten
 from keras.utils import to_categorical
 
+# -- k49 data -- 49 classifications
 x_train = np.load('data/k49-train-imgs.npz')['arr_0']
 y_train = np.load('data/k49-train-labels.npz')['arr_0']
 
-# -- data format
+x_test = np.load('data/k49-test-imgs.npz')['arr_0']
+y_test = np.load('data/k49-test-labels.npz')['arr_0']
+classifications = 49
+# -----
+
+# # -- basic kmnist dataset -- 10 classifications
+# x_train = np.load('data/kmnist-train-imgs.npz')['arr_0']
+# y_train = np.load('data/kmnist-train-labels.npz')['arr_0']
+
+# x_test = np.load('data/kmnist-test-imgs.npz')['arr_0']
+# y_test = np.load('data/kmnist-test-labels.npz')['arr_0']
+# classifications = 10
+# # -----
+
+# -- train data format
 img_rows, img_cols = 28, 28
 input_shape = (img_rows, img_cols, 1)
 
@@ -18,7 +33,16 @@ x_train /= 255
 
 x_train = x_train.reshape(x_train.shape[0], *input_shape)
 
-y_train = to_categorical(y_train, 49)
+y_train = to_categorical(y_train, classifications)
+
+# -- test data format
+
+x_test = x_test.astype('float32')
+x_test /= 255
+
+x_test = x_test.reshape(x_test.shape[0], *input_shape)
+
+y_test = to_categorical(y_test, classifications)
 # ---
 
 model = Sequential()
@@ -31,13 +55,15 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(49, activation='softmax'))
+model.add(Dense(classifications, activation='softmax'))
 
 model.compile(optimizer='adadelta',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3, verbose=1)
+model.fit(x_train, y_train, verbose=1,
+          batch_size=128, epochs=12,
+          validation_data=(x_test, y_test))
 
 model.save('model.h5')
 
