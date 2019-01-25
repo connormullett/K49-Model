25ea3566f5e8760cea5e937fd5fa367f536c902f
connor <connormullett@gmail.com>
2019-01-25 17:27:18 -0500

    seperated models need to retrain k49

--- evaluate.py
+++ evaluate.py
@@ -9,26 +9,27 @@ import pandas as pd
 
 img_rows, img_cols = 28, 28
 input_shape = (img_rows, img_cols, 1)
-class_map = pd.read_csv('data/k49_classmap.csv')
+class_map = pd.read_csv('data/kmnist_classmap.csv')
 
 # -- Data format
-x_test = np.load('data/k49-test-imgs.npz')['arr_0']
-y_test = np.load('data/k49-test-labels.npz')['arr_0']
+# x_test = np.load('data/k49-test-imgs.npz')['arr_0']
+# y_test = np.load('data/k49-test-labels.npz')['arr_0']
+
+x_test = np.load('data/kmnist-test-imgs.npz')['arr_0']
+y_test = np.load('data/kmnist-test-labels.npz')['arr_0']
+
+classifications = 10
 
 x_test = x_test.astype('float32')
 x_test /= 255
 x_test = x_test.reshape(x_test.shape[0], *input_shape)
-y_test = to_categorical(y_test, 49)
-# --
+y_test = to_categorical(y_test, classifications)
 
-model = keras.models.load_model('model.h5')
+model = keras.models.load_model('kmnist_model.h5')
 
-predictions = model.predict(x_test[:1000], verbose=1)
-prediction_test = predictions[0]
+# predictions = model.predict(x_test[:1000], verbose=1)
+# prediction_test = predictions[0]
 
 loss, acc = model.evaluate(x_test, y_test)
 print('acc: {}'.format(100 * acc))
-
-csv = pd.read_csv('data/k49_classmap.csv')
-print(csv.head(3))
 
